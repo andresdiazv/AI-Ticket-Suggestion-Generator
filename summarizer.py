6abace1f7577e91aaf50a4681b01d47e7a8c34c1
unknown <adiaz616@fiu.edu>
2025-07-30 21:22:46 -0400

    full suggestion sent to database

--- summarizer.py
+++ summarizer.py
@@ -16,14 +16,32 @@ cursor = conn.cursor(dictionary=True)
 cursor.execute("SELECT * FROM ticket WHERE ticket_status = 'open'")
 results = cursor.fetchall()
 
-for row in results:
-    issue_description = row.get("issue_description")
-
-response = client.responses.create(
-    model="gpt-4.1-nano",
-    input="Give a simple one line suggestion to help resolve the following issue:" + issue_description
+insert_suggestion_query = (
+  "INSERT INTO ticket_suggestions (ticket_id, ticket_suggestion)"
+  "VALUES (%s, %s)"
 )
 
-print(response.output_text)
+alter_status_query = (
+    "UPDATE ticket "
+    "SET ticket_status = 'closed' "
+    "WHERE ticket_id = %s"
+)
+
+#     print(response.output_text)
+for row in results:
+    ticket_id = row.get("ticket_id")
+    issue_description = row.get("issue_description")
+    response_suggestion = client.responses.create(
+    model="gpt-4.1-nano",
+    input="Give a simple one line suggestion to help resolve the following issue:" + issue_description)
+    suggestion_data = (ticket_id, response_suggestion.output_text)
+    status_data = (ticket_id,)
+    cursor.execute(insert_suggestion_query, suggestion_data)
+    cursor.execute(alter_status_query, status_data)
+    print(f"Processed ticket #{ticket_id} with GPT suggestion.")
+    conn.commit()
+
+
+
 cursor.close()
 conn.close()
